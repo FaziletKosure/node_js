90d700b5542bd0731515ec88d30b122c177ec757
FaziletKosure <faziletkosure1@gmail.com>
2020-12-01 11:12:50 +0100

    add writeFile

--- fileStream.js
+++ fileStream.js
@@ -1,8 +1,17 @@
 let fs = require('fs')
-
+//readFile
 fs.readFile('dosya.txt',"utf8",function (error,data) {
  if(error){
      throw error;
  } 
  console.log(data); //.toString() 
 })
+
+//WriteFile
+
+fs.writeFile('dosya2.txt',"Fazilet Kosure",function (error) {
+    if(error){
+        throw error;
+    } 
+    console.log("yazildi"); //.toString() 
+   })
