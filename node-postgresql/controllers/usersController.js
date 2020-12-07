09afb34364832ff0dc78e3fc8ad9566acf3ed69e
FaziletKosure <faziletkosure1@gmail.com>
2020-12-07 01:25:20 +0100

    add

--- node-postgresql/controllers/usersController.js
+++ node-postgresql/controllers/usersController.js
@@ -3,7 +3,11 @@ const UserModel = require("../models/User");
 exports.get_users = async (req, res, next) => {
   //get users from db
   try {
-    const userList = await UserModel.findAll({});
+    const userList = await UserModel.findAll({
+      order: [
+        ['id', 'ASC'],
+    ]
+    });
     res.render("users", { userList });
   } catch (error) {
     res.send("An error occured");
