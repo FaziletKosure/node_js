cfc6d8710d4f10c7e7666000bb024e82517c3806
FaziletKosure <faziletkosure1@gmail.com>
2020-12-07 01:01:41 +0100

    add node project

---
+++ node-postgresql/controllers/usersController.js
@@ -0,0 +1,68 @@
+const UserModel = require("../models/User");
+
+exports.get_users = async (req, res, next) => {
+  //get users from db
+  try {
+    const userList = await UserModel.findAll({});
+    res.render("users", { userList });
+  } catch (error) {
+    res.send("An error occured");
+  }
+};
+
+//on get request
+exports.show_add_user_form = (req, res) => {
+  res.render("addUser");
+};
+
+//on post request
+exports.add_user = async (req, res) => {
+  // add to db
+  try {
+    const newUser = await UserModel.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+    });
+    res.redirect("/users");
+  } catch (error) {
+    res.send("An error occured.");
+  }
+};
+
+//on delete request
+exports.delete_user = async (req, res) => {
+  try {
+    await UserModel.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.redirect("/users");
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+ //get  a user from db
+ exports.get_user = async (req, res, next) => {
+  try {
+    const user = await UserModel.findAll( {where: { id: req.params.id},
+    });
+    res.render("editUser", { user });
+  } catch (error) {
+    res.send("An error occured");
+  }
+};
+
+// on edit request
+exports.edit_user = async (req, res) => {
+  try {
+    await UserModel.update({firstName: req.body.firstName,lastName: req.body.lastName}, {where: { id: req.params.id}
+    });
+    res.redirect("/users");
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+
---
+++ node-postgresql/routes/users.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var router = express.Router();
+const usersController = require("../controllers/usersController");
+
+/*  /users */
+
+router.get("/", usersController.get_users);
+
+router.get("/add", usersController.show_add_user_form);
+router.post("/add", usersController.add_user);
+
+router.get("/:id/delete", usersController.delete_user);
+
+router.get("/:id/edit", usersController.get_user);
+router.post("/:id/edit", usersController.edit_user);
+
+
+module.exports = router;
